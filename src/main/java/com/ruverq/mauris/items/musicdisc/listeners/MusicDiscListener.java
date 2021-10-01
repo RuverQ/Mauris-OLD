d6eae6de3f9375511703e1881bfcd19193148a6f
RuverQ <ruqalex@gmail.com>
2021-10-01 22:27:28 +0300

    Fixed executing music when jukebox is broken

--- src/main/java/com/ruverq/mauris/items/musicdisc/listeners/MusicDiscListener.java
+++ src/main/java/com/ruverq/mauris/items/musicdisc/listeners/MusicDiscListener.java
@@ -12,11 +12,32 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public class MusicDiscListener implements Listener {
+
+    @EventHandler
+    public void onJukeboxBreak(BlockBreakEvent e){
+        Block cblock = e.getBlock();
+        if(cblock.getType() != Material.JUKEBOX) return;
+
+        if(DataContainerUtil.hasData(cblock, "mauris_musicdisc")) {
+            String discName = DataContainerUtil.getData(cblock, "mauris_musicdisc");
+            MaurisMusicDisc discMauris = (MaurisMusicDisc) ItemsLoader.getMaurisItem(discName);
+            Location ejectLocation = cblock.getLocation().add(0.5,1,0.5);
+            ejectLocation.getWorld().dropItemNaturally(ejectLocation, discMauris.getAsItemStack());
+            DataContainerUtil.clearData(cblock);
+            for(Entity entity : ejectLocation.getWorld().getNearbyEntities(ejectLocation, 15, 15, 15)){
+                if(!(entity instanceof Player)) return;
+                Player p = (Player) entity;
+                p.stopSound(discMauris.getMusic(), SoundCategory.RECORDS);
+            }
+            return;
+        }
+    }
 
     @EventHandler
     public void onDiscInsert(PlayerInteractEvent e){
